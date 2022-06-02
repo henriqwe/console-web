f75b5cfec626914ba845f2df9ad0473667eb65d1
Alex Ferreira Santos <alexfstos@gmail.com>
2022-06-02 20:30:43 -0300

    feat: finishing header

--- src/common/Header/index.tsx
+++ src/common/Header/index.tsx
@@ -22,8 +22,8 @@ export function Header() {
   ];
 
   return (
-    <div className="relative flex items-start w-full bg-gray-600">
-      <div className="flex items-center gap-4 w-[20%] px-6 py-1.5">
+    <div className="relative flex items-start w-full bg-gray-600 h-[3.4rem]">
+      <div className="flex items-center gap-4 w-[20%] px-6 py-1.5 ">
         <img
           src="http://www.ycodify.com/images/logo.png"
           className="w-[70%]"
@@ -31,7 +31,7 @@ export function Header() {
         />
         <p className="text-xs text-gray-200">V1.0.0</p>
       </div>
-      <div className="flex items-center justify-center pt-4 border-l border-gray-400">
+      <div className="flex items-center justify-center border-l border-gray-400 h-[3.4rem]">
         {headerSections.map((section) => (
           <HeaderSection
             key={section.title}
@@ -63,7 +63,7 @@ export function HeaderSection({
   return (
     <Link to={route} className="relative">
       <div
-        className={`flex items-center gap-1 px-3 pb-6 cursor-pointer ${
+        className={`flex items-center gap-1 px-3 cursor-pointer h-[3.4rem] ${
           active
             ? "after:block after:absolute after:h-1 after:bottom-0 after:left-0 after:bg-yellow-400 after:w-full text-yellow-400 "
             : "text-gray-200"
