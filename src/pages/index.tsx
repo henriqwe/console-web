2d60364c39a3dd665ed0a6c693d6f7608a41c4ff
henriqwe <henriqwe96@gmail.com>
2022-06-18 15:35:38 -0300

    feat: adicionando a extensão do javascript codemirror

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,22 @@
 /* This example requires Tailwind CSS v2.0+ */
-import * as data from "domains/console";
-import * as common from "common";
-import { Icon } from "@iconify/react";
-import CodeMirror from "@uiw/react-codemirror";
-import {
-  HomeIcon,
-  DatabaseIcon,
-  UserIcon,
-} from "@heroicons/react/outline";
+import * as data from 'domains/console'
+import * as common from 'common'
+import { Icon } from '@iconify/react'
+import CodeMirror from '@uiw/react-codemirror'
+import { HomeIcon, DatabaseIcon, UserIcon } from '@heroicons/react/outline'
+import { javascript } from '@codemirror/lang-javascript'
 
 const user = {
-  name: "Emily Selman",
-  email: "emily.selman@example.com",
+  name: 'Emily Selman',
+  email: 'emily.selman@example.com',
   imageUrl:
-    "https://images.unsplash.com/photo-1502685104226-ee32379fefbe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-};
+    'https://images.unsplash.com/photo-1502685104226-ee32379fefbe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
+}
 const navigation = [
-  { name: "Home", href: "http://localhost:3000", icon: HomeIcon },
-  { name: "Data", href: "#", icon: DatabaseIcon },
-  { name: "Users", href: "http://localhost:3000/user", icon: UserIcon },
-];
+  { name: 'Home', href: 'http://localhost:3000', icon: HomeIcon },
+  { name: 'Data', href: '#', icon: DatabaseIcon },
+  { name: 'Users', href: 'http://localhost:3000/user', icon: UserIcon }
+]
 
 export default function Example() {
   return (
@@ -39,11 +36,12 @@ export default function Example() {
             <div className="flex flex-col bg-gray-200 border-2">
               <div>
                 <CodeMirror
-                  value={`# Looks like you do not have any tables.\n# Click on the 'Data' tab on top to create tables\n# Try out YCode queries here after you create tables;`}
+                  value={`// Looks like you do not have any tables.\n// Click on the 'Data' tab on top to create tables\n// Try out YCode queries here after you create tables;`}
                   height="50vh"
                   onChange={(value, viewUpdate) => {
-                    console.log("value:", value);
+                    console.log('value:', value)
                   }}
+                  extensions={[javascript({ jsx: true })]}
                 />
               </div>
               <div className="flex w-full p-2 pb-0 pl-8 bg-gray-200 ring-1 ring-gray-300">
@@ -54,8 +52,9 @@ export default function Example() {
                   value=""
                   height="35vh"
                   onChange={(value, viewUpdate) => {
-                    console.log("value:", value);
+                    console.log('value:', value)
                   }}
+                  extensions={[javascript({ jsx: true })]}
                 />
               </div>
             </div>
@@ -69,14 +68,15 @@ export default function Example() {
               value=""
               height="90vh"
               onChange={(value, viewUpdate) => {
-                console.log("value:", value);
+                console.log('value:', value)
               }}
               editable={false}
+              extensions={[javascript({ jsx: true })]}
             />
           </div>
         </section>
         <data.DataSideBar />
       </common.Template>
     </data.DataProvider>
-  );
+  )
 }
