46b44b1be026c4f517ab21e43dcfe117235c79ae
henriqwe <henriqwe96@gmail.com>
2022-07-12 20:06:36 -0300

    refactor: removendo o editor Query variables

--- src/domains/console/Console/Editors/Editors.tsx
+++ src/domains/console/Console/Editors/Editors.tsx
@@ -26,9 +26,9 @@ export function Editors() {
 
   return (
     <div className="flex w-full h-full rounded-lg">
-      <div className="w-[50%] max-h-[77vh] rounded-lg h-full">
+      <div className="w-[50%] rounded-lg h-full">
         <div className="flex flex-col h-full rounded-lg">
-          <div className="flex flex-col bg-gray-200 rounded-tl-lg h-3/4">
+          <div className="flex flex-col bg-gray-200 rounded-tl-lg h-full">
             <div className="flex items-center justify-between w-full pl-4 border-r min-h-[3.5rem] max-h-[3.5rem] border-r-gray-300">
               <p className="text-lg font-bold text-gray-700">YCode Console</p>
               <div className="flex h-full">
@@ -58,7 +58,7 @@ export function Editors() {
             <div className="flex w-full h-full overflow-x-auto ">
               <CodeMirror
                 value={consoleValue}
-                className="flex w-full h-ful"
+                className="flex w-full h-full"
                 width="100%"
                 onChange={(value, viewUpdate) => {
                   setConsoleValue(value)
@@ -70,29 +70,12 @@ export function Editors() {
               />
             </div>
           </div>
-          <div className="flex flex-col h-1/4">
-            <div className="flex items-center w-full px-4 bg-gray-200 h-14">
-              <p className="text-lg font-bold text-gray-700">Query variables</p>
-            </div>
-            <div className="flex w-full h-full overflow-x-auto rounded-bl-lg">
-              <CodeMirror
-                value=""
-                className="flex w-full h-ful"
-                width="100%"
-                onChange={(value, viewUpdate) => {
-                  console.log('value:', value)
-                }}
-                extensions={[javascript({ jsx: true })]}
-              />
-            </div>
-          </div>
         </div>
       </div>
       <div className="w-[50%] h-full flex flex-col">
         <div className="flex items-center w-full h-16 px-4 bg-gray-200 rounded-tr-lg">
           <div className="flex justify-between items-center w-full">
             <p className="text-lg font-bold text-gray-700">Response</p>
-
             <div>
               {consoleResponseFormated && (
                 <button
