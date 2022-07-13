9e8ee72a0d61074c8fa88a84dbab07a9348d0097
henriqwe <henriqwe96@gmail.com>
2022-07-12 23:10:07 -0300

    feat: adicionando o icone de retorno na lib common

---
+++ src/common/Icons/ReturnIcon/ReturnIcon.tsx
@@ -0,0 +1,7 @@
+import { Icon } from '@iconify/react'
+
+export function ReturnIcon({ className }: { className?: string }) {
+  return (
+    <Icon icon="icon-park-outline:return" className={`w-3 h-3 ${className}`} />
+  )
+}
