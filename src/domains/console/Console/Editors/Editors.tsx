be7edb1415996b2cf67cf368469dc8a240a0fdae
henriqwe <henriqwe96@gmail.com>
2022-07-12 21:47:57 -0300

    refactor: alterando o texto run para o de play

--- src/domains/console/Console/Editors/Editors.tsx
+++ src/domains/console/Console/Editors/Editors.tsx
@@ -1,5 +1,6 @@
 import CodeMirror from '@uiw/react-codemirror'
 
+import { Icon } from '@iconify/react'
 import { javascript } from '@codemirror/lang-javascript'
 import { useState } from 'react'
 import { DatabaseIcon } from '@heroicons/react/outline'
@@ -21,62 +22,67 @@ export function Editors() {
     consoleResponseFormated,
     setConsoleValue,
     consoleResponseLoading,
-    documentationValue
+    documentationValue,
+    responseTime
   } = consoleEditor.useConsoleEditor()
 
   return (
     <div className="flex w-full h-full rounded-lg">
       <div className="w-[50%] rounded-lg h-full">
-        <div className="flex flex-col h-full rounded-lg">
-          <div className="flex flex-col bg-gray-200 rounded-tl-lg h-full">
-            <div className="flex items-center justify-between w-full pl-4 border-r min-h-[3.5rem] max-h-[3.5rem] border-r-gray-300">
-              <p className="text-lg font-bold text-gray-700">YCode Console</p>
-              <div className="flex h-full">
-                <button
-                  className="flex items-center h-full gap-2 p-2 text-blue-400 border-gray-400 border-x"
+        <div className="flex flex-col bg-gray-200 rounded-tl-lg h-full">
+          <div className="flex items-center justify-between w-full pl-4 border-r h-9 border-r-gray-300">
+            <p className="text-base  text-gray-900">YCodi Console</p>
+            <div className="flex h-full">
+              <button
+                className="flex items-center h-full gap-2 p-2 "
+                title="Run"
+                onClick={runOperation}
+                disabled={consoleResponseLoading}
+              >
+                <div
+                  className="flex items-center justify-center rounded-full border-2 border-gray-400 w-9 h-9"
                   title="Run"
-                  onClick={runOperation}
-                  disabled={consoleResponseLoading}
                 >
-                  Run
-                  {consoleResponseLoading && (
-                    <div className="w-4 h-4">
-                      <common.Spinner />
-                    </div>
-                  )}
-                </button>
-                <button
-                  className="flex items-center h-full gap-2 p-2 text-blue-400 border-gray-400 border-x"
-                  title="Docs"
-                  onClick={() => setSlideOpen(!slideOpen)}
-                  disabled={consoleResponseLoading}
-                >
-                  Docs
-                </button>
-              </div>
+                  <Icon
+                    icon="fa-solid:play"
+                    className={`ml-1 w-4 h-4 text-gray-700`}
+                  />
+                </div>
+              </button>
+              <button
+                className="flex items-center h-full gap-2 p-2 text-blue-400 border-gray-400 border-x"
+                title="Docs"
+                onClick={() => setSlideOpen(!slideOpen)}
+                disabled={consoleResponseLoading}
+              >
+                Docs
+              </button>
             </div>
-            <div className="flex w-full h-full overflow-x-auto ">
-              <CodeMirror
-                value={consoleValue}
-                className="flex w-full h-full"
-                width="100%"
-                onChange={(value, viewUpdate) => {
-                  setConsoleValue(value)
-                }}
-                extensions={[
-                  javascript({ jsx: true }),
-                  globalJavaScriptCompletions
-                ]}
-              />
-            </div>
+          </div>
+          <div className="flex w-full h-full overflow-x-auto ">
+            <CodeMirror
+              value={consoleValue}
+              className="flex w-full h-full"
+              width="100%"
+              onChange={(value, viewUpdate) => {
+                setConsoleValue(value)
+              }}
+              extensions={[
+                javascript({ jsx: true }),
+                globalJavaScriptCompletions
+              ]}
+            />
           </div>
         </div>
       </div>
       <div className="w-[50%] h-full flex flex-col">
-        <div className="flex items-center w-full h-16 px-4 bg-gray-200 rounded-tr-lg">
+        <div className="flex items-center w-full h-9 px-4 bg-gray-200 rounded-tr-lg">
           <div className="flex justify-between items-center w-full">
-            <p className="text-lg font-bold text-gray-700">Response</p>
-            <div>
+            <p className="text-base  text-gray-900">Response</p>
+            <div className="flex gap-2 items-center">
+              {responseTime && (
+                <div className="text-xs">Response time: {responseTime} ms</div>
+              )}
               {consoleResponseFormated && (
                 <button
                   className="px-2 py-1 text-sm bg-white border border-gray-300 rounded-md"
@@ -97,13 +103,22 @@ export function Editors() {
           </div>
         </div>
         <div className="flex w-full h-full overflow-x-auto rounded-br-lg">
-          <CodeMirror
-            value={consoleResponseFormated}
-            className="flex w-full h-full"
-            width="100%"
-            editable={false}
-            extensions={[javascript({ jsx: true })]}
-          />
+          {consoleResponseLoading ? (
+            <div className="flex justify-center items-center w-full h-full">
+              <div className="flex gap-2 items-center">
+                <div>Loading</div>
+                <common.Spinner className="w-5 h-5" />
+              </div>
+            </div>
+          ) : (
+            <CodeMirror
+              value={consoleResponseFormated}
+              className="flex w-full h-full"
+              width="100%"
+              editable={false}
+              extensions={[javascript({ jsx: true })]}
+            />
+          )}
         </div>
       </div>
       <common.Slide
