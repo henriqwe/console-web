688a830b1ddcf1ca5963edf10969da3d814b2ea3
Gilvan Júnior <gcostajr@gmail.com>
2022-07-16 16:14:21 -0300

    refactor: :lipstick: ajustes na UI do dashboard

--- src/domains/dashboard/Template/Template.tsx
+++ src/domains/dashboard/Template/Template.tsx
@@ -41,128 +41,14 @@ export function Template({ children }: TemplateProps) {
   return (
     <>
       <div>
-        <Transition.Root show={sidebarOpen} as={Fragment}>
-          <Dialog
-            as="div"
-            className="relative z-40 md:hidden"
-            onClose={setSidebarOpen}
-          >
-            <Transition.Child
-              as={Fragment}
-              enter="transition-opacity ease-linear duration-300"
-              enterFrom="opacity-0"
-              enterTo="opacity-100"
-              leave="transition-opacity ease-linear duration-300"
-              leaveFrom="opacity-100"
-              leaveTo="opacity-0"
-            >
-              <div className="fixed inset-0 bg-gray-600 bg-opacity-75" />
-            </Transition.Child>
-
-            <div className="fixed inset-0 z-40 flex">
-              <Transition.Child
-                as={Fragment}
-                enter="transition ease-in-out duration-300 transform"
-                enterFrom="-translate-x-full"
-                enterTo="translate-x-0"
-                leave="transition ease-in-out duration-300 transform"
-                leaveFrom="translate-x-0"
-                leaveTo="-translate-x-full"
-              >
-                <Dialog.Panel className="relative flex flex-col flex-1 w-full max-w-xs bg-gray-800">
-                  <Transition.Child
-                    as={Fragment}
-                    enter="ease-in-out duration-300"
-                    enterFrom="opacity-0"
-                    enterTo="opacity-100"
-                    leave="ease-in-out duration-300"
-                    leaveFrom="opacity-100"
-                    leaveTo="opacity-0"
-                  >
-                    <div className="absolute top-0 right-0 pt-2 -mr-12">
-                      <button
-                        type="button"
-                        className="flex items-center justify-center w-10 h-10 ml-1 rounded-full focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-                        onClick={() => setSidebarOpen(false)}
-                      >
-                        <span className="sr-only">Close sidebar</span>
-                        <XIcon
-                          className="w-6 h-6 text-white"
-                          aria-hidden="true"
-                        />
-                      </button>
-                    </div>
-                  </Transition.Child>
-                  <div className="flex-1 h-0 pt-5 pb-4 overflow-y-auto">
-                    <div className="flex items-center flex-shrink-0 px-4">
-                      <img
-                        className="w-auto h-8"
-                        src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg"
-                        alt="Workflow"
-                      />
-                    </div>
-                    <nav className="px-2 mt-5 space-y-1">
-                      {navigation.map((item) => (
-                        <a
-                          key={item.name}
-                          href={item.href}
-                          className={`${
-                            item.current
-                              ? 'bg-gray-900 text-white'
-                              : 'text-gray-300 hover:bg-gray-700'
-                          } group flex items-center px-2 py-2 text-base font-medium rounded-md`}
-                        >
-                          <item.icon
-                            className={`mr-4 flex-shrink-0 h-6 w-6 ${
-                              item.current
-                                ? 'text-gray-300'
-                                : 'text-gray-400 group-hover:text-gray-300'
-                            }`}
-                            aria-hidden="true"
-                          />
-                          {item.name}
-                        </a>
-                      ))}
-                    </nav>
-                  </div>
-                  <div className="flex flex-shrink-0 p-4 bg-gray-700">
-                    <a href="#" className="flex-shrink-0 block group">
-                      <div className="flex items-center">
-                        <div>
-                          <img
-                            className="inline-block w-10 h-10 rounded-full"
-                            src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                            alt=""
-                          />
-                        </div>
-                        <div className="ml-3">
-                          <p className="text-base font-medium text-white">
-                            Tom Cook
-                          </p>
-                          <p className="text-sm font-medium text-gray-400 group-hover:text-gray-300">
-                            View profile
-                          </p>
-                        </div>
-                      </div>
-                    </a>
-                  </div>
-                </Dialog.Panel>
-              </Transition.Child>
-              <div className="flex-shrink-0 w-14">
-                {/* Force sidebar to shrink to fit close icon */}
-              </div>
-            </div>
-          </Dialog>
-        </Transition.Root>
-
         {/* Static sidebar for desktop */}
         <div className="hidden md:flex md:w-64 md:flex-col md:fixed md:inset-y-0">
           {/* Sidebar component, swap this element with another sidebar if you like */}
           <div className="flex flex-col flex-1 min-h-0 bg-gray-800">
-            <div className="flex flex-col flex-1 pt-5 pb-4 overflow-y-auto">
-              <div className="flex items-center flex-shrink-0 px-4">
+            <div className="flex flex-col flex-1 pt-5 pb-4 overflow-y-auto mx-4">
+              <div className="flex items-center flex-shrink-0 px-4 my-10">
                 <img
-                  className="w-auto h-8"
+                  className="w-auto h-7"
                   src="/assets/images/logo.png"
                   alt="Workflow"
                 />
@@ -172,14 +58,14 @@ export function Template({ children }: TemplateProps) {
                   <a
                     key={item.name}
                     href={item.href}
-                    className={`group flex items-center px-2 py-2 text-sm font-medium rounded-md transition ${
+                    className={`group flex items-center px-2 py-2 text-xs font-medium rounded-md transition ${
                       item.current
                         ? 'bg-gray-900 text-white'
                         : 'text-gray-300 hover:bg-gray-700 hover:text-white'
                     }`}
                   >
                     <item.icon
-                      className={`mr-3 flex-shrink-0 h-6 w-6 ${
+                      className={`mr-4 flex-shrink-0 h-6 w-6 ${
                         item.current
                           ? 'text-gray-300'
                           : 'text-gray-400 group-hover:text-gray-300'
@@ -200,7 +86,7 @@ export function Template({ children }: TemplateProps) {
                 }}
               >
                 <LogoutIcon
-                  className={`mr-3 flex-shrink-0 h-6 w-6 text-gray-400 group-hover:text-gray-300`}
+                  className={`mr-3 text-xs flex-shrink-0 h-6 w-6 text-gray-400 group-hover:text-gray-300`}
                   aria-hidden="true"
                 />
                 Logout
